ee68115ab0dd44d26eca89826d746d92a2499faf
Jakub Niepsuj <jakubniepsujj@gmail.com>
2024-03-01 09:13:24 +0100

    2. commit: post component

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+                    <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+                        {{ post }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+
+import { useUserStore } from "~/stores/user";
+const userStore = new useUserStore();
+//const user = new useSupabaseUser();
+
+let posts = ref([])
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+    posts.value = [
+        {
+            name: 'James Notdamage',
+            image: 'https://placehold.co/100',
+            text: 'This is the title',
+            picture: 'https://placehold.co/500',
+        }
+    ]
+})
+</script>
---
+++ stores/user.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia"
+
+export const useUserStore = defineStore("user", {
+    state: () => ({
+        posts: [],
+        isMenuOverlay: false,
+        isLogoutOvelay: false,
+    }),
+})
+
+
+
+
+
+
+// user.js
+// -> komponent przechowuje informacje o użytkowniku
+// -> przechowuje posty
+// -> flagi informujące czy nakładka menu i nakładka logout jest widoczna
+
+// Importuje funkcję defineStore z biblioteki vue-typed-store, która służy do definiowania Vuex store zgodnie z Composition API.
+// useUserStore: Jest to nazwa zmiennej, która przechowuje instancję stworzonego magazynu. Współczesne konwencje używają przedrostka use dla funkcji Vue Composition API.
+// defineStore("user", { ... }): Wywołuje funkcję defineStore i definiuje magazyn o nazwie "user". Pierwszym argumentem jest nazwa magazynu, a drugim argumentem jest obiekt zawierający definicję stanu, akcji itp.
+
+// state: Jest to funkcja fabrykująca, która zwraca początkowy stan magazynu.
+// posts: Jest to tablica, która przechowuje posty użytkownika (lub inne informacje).
+// isMenuOverlay: Jest to flaga mówiąca, czy overlay (nakładka) menu jest widoczna czy nie.
+// isLogoutOvelay: Jest to flaga mówiąca, czy overlay wylogowywania jest widoczna czy nie.
+
