59aab4d44c0fda2ea6dcbe6b77658fffdfa1f396
Jakub Niepsuj <jakubniepsujj@gmail.com>
2024-03-02 09:09:32 +0100

    6. commit: Menu Modal Overlay

--- app.vue
+++ app.vue
@@ -9,6 +9,13 @@
       ]"
     />
 
+    <Modal
+      :class="[
+          {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isLogoutOverlay },
+          {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isLogoutOverlay },
+      ]"
+    />
+
   </div>
 </template>
 
--- stores/user.js
+++ stores/user.js
@@ -4,7 +4,7 @@ export const useUserStore = defineStore("user", {
     state: () => ({
         posts: [],
         isMenuOverlay: false,
-        isLogoutOvelay: false,
+        isLogoutOverlay: false,
     }),
 })
 
